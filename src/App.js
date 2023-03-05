b7da1b731b793b4bc9c9d6062ea6e683004754df
hohoangphuc1907 <84660182+hohoangphuc1907@users.noreply.github.com>
2023-03-05 09:19:06 +0700

    update search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { SkeletonText } from "@chakra-ui/react";
-
+import React, { useState } from "react";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-
-const center = { lat: 10.892441327685136, lng: 106.68285931540154 };
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng, 
+} from "react-places-autocomplete";
+import './App.css';
 
 function App() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries: ["places"],
   });
-
+  const [address, setAddress] = useState("");
+  const [center, setCenter] = useState({ lat: 10.7913, lng: 106.7037 });
+  const [markers, setMarkers] = useState([]);
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setAddress(value);
+    setMarkers([...markers, latLng]);
+    setCenter(latLng);
+    
+  };
   if (!isLoaded) {
     return <SkeletonText />;
   }
@@ -21,7 +34,35 @@ function App() {
     window.open(`https://www.luburestaurant.com?coords=${lat},${lng}`);
   };
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
+    <div >
+       <PlacesAutocomplete
+        value={address}
+        onChange={(value) => setAddress(value)}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input {...getInputProps({ placeholder: "Nhập địa chỉ" })} />
+            <div>
+              {loading ? <div>Loading...</div> : null}
+              {suggestions.map((suggestion) => {
+                const style = {
+                  backgroundColor: suggestion.active ? "#41b6e6" : "#fff",
+                };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, { style })}
+                    key={suggestion.placeId}
+                  >
+                    {suggestion.description}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+      <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMap
         center={center}
         zoom={15}
@@ -33,8 +74,13 @@ function App() {
           fullscreenControl: false,
         }}
       >
-        <Marker position={center} onClick={handleClick} />
+        
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker} onClick={handleClick} />
+        ))}
       </GoogleMap>
+      </div>
+   
     </div>
   );
 }
